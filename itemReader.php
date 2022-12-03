aac4b3744e4968f8b9193fbe3aa1c15ca5d63c9e
Hasantha <hasantha.pathirana@hotmail.com>
2022-12-04 03:43:23 +0530

    fuel request ui and qr code continuation

---
+++ itemReader.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Read Items</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="./css/itemreader-style.css">
+        <link rel="stylesheet" type="text/css" href="./Resources/css/JsQRScanner.css">
+        <script type="text/javascript" src="./js/qrCodeJs/jsqrscanner.nocache.js"></script>
+        
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+        
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+            <div class="col-xl-6">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div style="height:750px" class="col -lg-5">
+                                <div  class="row justify-content-center pt-4">
+                                    <img class="img-fluid px-2" src="Resources/icons/logo.PNG" alt="brand">
+                                </div>
+                                <hr>
+                               <div class="row-element">
+                                    <div class="qrscanner" id="scanner">
+                                </div>
+                                </div>
+                          
+                                
+                                <hr>
+                                <div id="form-frame" class="px-5 pb-5 pt-4">
+                                    <form class="user" action="itemread-backend.php" method="post">
+                                        <div class="form-group"><input readonly name="text" id="scannedTextMemo" class="form-control read-item" type="text" placeholder="Waiting for an item" name="username"></div>
+                                        <input type="hidden" name="itemid" id="itemid">
+                                        <input type="hidden" name="itemname" id="itemname">
+                                        <input type="hidden" name="itemprice" id="itemprice">
+                                        <input type="hidden" name="itemquantity" id="itemquantity">
+                                        <input type="hidden" name="empid" id="empid" value="<?php echo $session ?>">
+                                        <button name="btn-submit" id="btn-submit" class="btn btn-block btn-user btn-cart" type="submit">Add to cart</button>
+                                        <hr>
+                                        
+
+                                         
+                                    </form>
+                                    <div class="text-center"><a  href="itemCart.php">View Cart</a></div>
+                                    
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        
+        </div>
+        
+       <script type="text/javascript">
+    function onQRCodeScanned(scannedText)
+    {
+    	var scannedTextMemo = document.getElementById("scannedTextMemo");
+        var itemid = document.getElementById("itemid");
+        var itemname = document.getElementById("itemname");
+        var itemprice = document.getElementById("itemprice");
+        var itemquantity = document.getElementById("itemquantity");
+    	if(scannedTextMemo)
+    	{
+            var itemArr=scannedText.split(",");
+    		scannedTextMemo.value =scannedText;
+            itemid.value=itemArr[0];
+            itemname.value=itemArr[1];
+            itemprice.value=itemArr[2];
+            itemquantity.value=itemArr[3];
+    	}
+    	
+    }
+    
+    function provideVideo()
+    {
+        var n = navigator;
+
+        if (n.mediaDevices && n.mediaDevices.getUserMedia)
+        {
+          return n.mediaDevices.getUserMedia({
+            video: {
+              facingMode: "environment"
+            },
+            audio: false
+          });
+        } 
+        
+        return Promise.reject('Your browser does not support getUserMedia');
+    }
+
+    function provideVideoQQ()
+    {
+        return navigator.mediaDevices.enumerateDevices()
+        .then(function(devices) {
+            var exCameras = [];
+            devices.forEach(function(device) {
+            if (device.kind === 'videoinput') {
+              exCameras.push(device.deviceId)
+            }
+         });
+            
+            return Promise.resolve(exCameras);
+        }).then(function(ids){
+            if(ids.length === 0)
+            {
+              return Promise.reject('Could not find a webcam');
+            }
+            
+            return navigator.mediaDevices.getUserMedia({
+                video: {
+                  'optional': [{
+                    'sourceId': ids.length === 1 ? ids[0] : ids[1]//this way QQ browser opens the rear camera
+                    }]
+                }
+            });        
+        });                
+    }
+    
+    //this function will be called when JsQRScanner is ready to use
+    function JsQRScannerReady()
+    {
+        //create a new scanner passing to it a callback function that will be invoked when
+        //the scanner succesfully scan a QR code
+        var jbScanner = new JsQRScanner(onQRCodeScanned);
+        //var jbScanner = new JsQRScanner(onQRCodeScanned, provideVideo);
+        //reduce the size of analyzed image to increase performance on mobile devices
+        jbScanner.setSnapImageMaxSize(300);
+    	var scannerParentElement = document.getElementById("scanner");
+    	if(scannerParentElement)
+    	{
+    	    //append the jbScanner to an existing DOM element
+    		jbScanner.appendTo(scannerParentElement);
+    	}        
+    }
+  </script>    
+       
+    </body>
+</html>
--- js/requestFuel.js
+++ js/requestFuel.js
@@ -11,11 +11,45 @@ $(document).ready(function(){
 
    $('.form-fuelRequest').submit(function(event){
       event.preventDefault();
-      var fuel_type=$('#fuel-type').val();
-      var request_amount=parseInt($('#requested-amount').val());
+      var station_id=5;
+      var requested_date=new Date().toISOString().slice(0, 10);
       var status="pending";
+      var batch_num = Math.ceil(new Date().getTime() / 1000);
+      var request_amount=parseInt($('#requested-amount').val());
       var received_quantity=0;
+      var dispatch_quantity=0;
+      var unit_price=12.50;
+      var fuel_type=$('#fuel-type').val();
 
-      console.log(fuel_type+" "+request_amount);
+      var apiData={
+         "station_id":6,
+         "request_date":requested_date,
+         "request_status":status,
+         "batch_num":batch_num,
+         "requested_quantity":request_amount,
+         "received_quantity":received_quantity,
+         "dispatched_quantity":dispatch_quantity,
+         "unit_price":unit_price,
+         "fuel_type":fuel_type
+      };
+
+      var url="http://localhost:8080/fuelRequest/insert";
+
+      $.ajax({
+         type:"POST",
+         url:url,
+         data:JSON.stringify(apiData),
+         crossDomain:true,
+         contentType:"application/json",
+         success:function(data){
+            console.log(data);
+
+            $('.form-fuelRequest').trigger("reset");
+            $("#fuel-request-modal").modal("hide");
+
+         }
+
+      })
+
    })
 });
