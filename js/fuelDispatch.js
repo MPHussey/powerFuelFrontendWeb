9bcf9aa132bde3702ec55ef2f6622d4b151af989
Jeewan198 <jeewan198@gmail.com>
2022-12-09 17:03:22 +0530

    1209evening

--- js/fuelDispatch.js
+++ js/fuelDispatch.js
@@ -42,3 +42,81 @@ function getAllAllocations(){
     })
     
 }
+
+
+$(document).ready(function () {
+    $(document).on('click', '#allocationTbl tr', function () {
+        var col0 = $(this).find('td:eq(0)').text();
+        var col2 = $(this).find('td:eq(2)').text();
+        var col3 = $(this).find('td:eq(3)').text();
+        var col5 = $(this).find('td:eq(5)').text();
+       
+        
+        $('#dispatch-allocation-id').val(col0);
+        $('#dispatch-Fuel-Quantity').val(col2);
+        $('#dispatch-fuelType').val(col3);
+        $('#dispatch-stationId').val(col5);
+        
+
+    })
+})
+
+
+
+
+function dispatchFuel(){
+
+    let allocationId = $('#dispatch-allocation-id').val();
+    let dispatchDate = $('#dispatch-date').val();
+    let deliveryTime = $('#dispatch-deliveryTime').val();
+    let fuelQty = $('#dispatch-Fuel-Quantity').val();
+    let dispatchStatus = $('#dispatch-status').val();
+    let transportUnit = $('#dispatch-transportUnit').val();
+    let dispatchStationId = $('#dispatch-stationId').val();
+    let dispatchFuelType= $('#dispatch-fuelType').val();
+
+
+    console.log(dispatchDate);
+
+
+
+    
+    $.ajax({
+        method:"POST",
+        contentType:"application/json",
+        url:"http://localhost:8080/fuelDispatch/saveFuelDispatch2",
+        async:true,
+        data:JSON.stringify({
+
+            
+
+            "dispatchID":"",
+            "allocationID":allocationId,
+            "dispatchDate":dispatchDate,
+            "transportUnit":transportUnit,
+            "expectedDeliveryTime":deliveryTime,
+            "fuelQty":fuelQty,
+            "status":dispatchStatus,
+            "stationId":dispatchStationId,
+            "fuelType":dispatchFuelType
+
+        }),
+        success:function(data){
+            alert("Fuel dispatched to the station id "+dispatchStationId)
+        },
+        error:function(xhr,exception){
+            alert("Error")
+        }
+
+
+    })
+    
+}
+
+
+
+
+
+
+
+
--- pages/fuel-dispatch.php
+++ pages/fuel-dispatch.php
@@ -42,14 +42,58 @@
                     </div>
                 </div>
 
-                <div class="container mt-5 col-6">
+                <div class="container mt-1 col-10 ">
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <label for="dispatch-dispatchId" class="form-label">Dispatch ID</label>
+                        <input type="text" disabled class="form-control" id="dispatch-dispatchId" placeholder="Dispatch ID">
+                    </div>
+                    <div class="mb-3">
+                        <label for="dispatch-allocation-id" class="form-label">Allocation Id</label>
+                        <input type="text" class="form-control" id="dispatch-allocation-id" placeholder="Allocation Id">
+                    </div>
+                    <div class="mb-3">
+                        <label for="dispatch-date" class="form-label">Dispatched Date</label>
+                        <input type="date" class="form-control" id="dispatch-date" placeholder="Date">
+                    </div>
+                    
+                   <div class="mb-3">
+                        <label for="dispatch-deliveryTime" class="form-label">Delivery Time</label>
+                        <input type="text" class="form-control" id="dispatch-deliveryTime" placeholder="Delivery Time">
+                    </div>
+                    <div class="mb-3">
+                        <label for="dispatch-Fuel-Quantity" class="form-label">Fuel Quantity</label>
+                        <input type="text" class="form-control" id="dispatch-Fuel-Quantity" placeholder="Fuel Quantity">
+                    </div>
+                    <div class="mb-3">
+                        <label for="dispatch-stationId" class="form-label">Station Id</label>
+                        <input type="text" class="form-control" id="dispatch-stationId" placeholder="Station Id">
+                    </div>
+                    <div class="mb-3">
+                        <label for="dispatch-fuelType" class="form-label">Fuel Type</label>
+                        <input type="text" class="form-control" id="dispatch-fuelType" placeholder="Fuel Type">
+                    </div>
+                    <div class="mb-3">
+                    <label for="vehicle-make">Fuel Dispatch Status</label>
+                            <select name="fuel-type" id="dispatch-status" class="form-control">
+                                <option value="-">Select Status</option>
+                                <option value="1">Pending</option>
+                                <option value="2">Dispatched</option>
+                                <option value="3">Allocated</option>
+                            </select>                        
+                    </div> 
+                    <div class="mb-3">
+
+                            <label for="vehicle-make">Transport Unit</label>
+                            <select name="fuel-type" id="dispatch-transportUnit" class="form-control">
+                                <option value="-">Select Status</option>
+                                <option value="1">Lorry</option>
+                                <option value="2">Van</option>
+                            </select>
+                    </div> 
+                    <div>
+                    <button type="button" class="btn btn-danger" onclick="dispatchFuel()">Dispatch</button>
                     </div>
                 </div> 
-
-                
 
 
 
